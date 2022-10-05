0ecfd75553c68ba4fb62efaed5e420c695906cb7
Danielateresa <dany.t_87@hotmail.it>
2022-10-05 15:30:32 +0200

    Added bootstrap  with some style and ; on js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
-const yourName = prompt('Scrivi il tuo nome')
-console.log(yourName)
+const yourName = prompt('Scrivi il tuo nome');
+console.log(yourName);
 
-const yourSurname = prompt('Scrivi il tuo cognome')
-console.log(yourSurname)
+const yourSurname = prompt('Scrivi il tuo cognome');
+console.log(yourSurname);
 
-const favouriteColor = prompt('Scrivi il tuo colore preferito')
-console.log(favouriteColor)
+const favouriteColor = prompt('Scrivi il tuo colore preferito');
+console.log(favouriteColor);
 
-document.getElementById('password').innerHTML = `La tua password insicurissima è: ${yourName}${yourSurname}${favouriteColor}22`
+document.getElementById('password').innerHTML = `La tua password insicurissima è: ${yourName}${yourSurname}${favouriteColor}22`;
