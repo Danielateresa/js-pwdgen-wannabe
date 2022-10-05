6db38ea6baf54c67d4e49d49341facde628dcd5f
Danielateresa <dany.t_87@hotmail.it>
2022-10-05 13:26:54 +0200

    Added variables for the three questions

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+const yourName = prompt('Scrivi il tuo nome')
+console.log(yourName)
+
+const yourSurname = prompt('Scrivi il tuo cognome')
+console.log(yourSurname)
+
+const favouriteColor = prompt('Scrivi il tuo colore preferito')
+console.log(favouriteColor)
